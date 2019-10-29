7713a3f0cbcc5c87261d91a6e792213b750f7cfc
Marc Brinkmann <git@git.marcbrinkmann.de>
2019-10-29 14:55:36 +0100

    docs: add doctest with example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
 //! Serde PHP serialization format support.
 //!
 //! See `README.md` for details.
+//!
+//! # Example
+//!
+//! ```rust
+//! use serde::Deserialize;
+//! use serde_php::from_bytes;
+//!
+//! #[derive(Debug, Deserialize, Eq, PartialEq)]
+//! struct UserProfile {
+//!     id: u32,
+//!     name: String,
+//!     tags: Vec<String>,
+//! }
+//!
+//! let serialized = br#"a:3:{s:2:"id";i:42;s:4:"name";s:3:"Bob";s:4:"tags";a:2:{i:0;s:3:"foo";i:1;s:3:"bar";}}"#;
+//!
+//! let profile: UserProfile = from_bytes(serialized).expect("deserialization failed");
+//! ```
 
 pub mod de;
 
