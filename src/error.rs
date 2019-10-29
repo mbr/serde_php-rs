68204d9e819e449076c9462902cb3eb810a9003d
Marc Brinkmann <git@git.marcbrinkmann.de>
2019-10-29 19:29:58 +0100

    refactor: use `thiserror` and `displaydoc` to generate error type

--- src/error.rs
+++ src/error.rs
@@ -1,20 +1,25 @@
+//! Top-level error type for PHP serialization/deserialization.
+
+use displaydoc::Display;
 use std::{fmt, io};
+use thiserror::Error;
 
-#[derive(Debug)]
+/// PHP serialization/deserialization error.
+#[derive(Error, Debug, Display)]
 pub enum Error {
-    Custom(String),
+    /// Error serializing value: {0}
     WriteSerialized(io::Error),
+    /// Feature not implemented by `serde_php`: {0}
     MissingFeature(&'static str),
+    /// Attempted to serialize sequence of unknown length.
+    ///
+    /// PHP requires all collections to be length prefixed. Serializing
+    /// sequences of unknown length requires writing these to a memory buffer
+    /// with potentially unbounded space requirements and is thus disabled.
     LengthRequired,
+    /// PHP Serialization error: {0}
+    Custom(String),
 }
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "TODO")
-    }
-}
-
-impl std::error::Error for Error {}
 
 impl serde::ser::Error for Error {
     fn custom<T>(msg: T) -> Self
